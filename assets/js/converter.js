8ec057339a7d0cd0d7888830b348cfaa0dc89c78
PeterDBR27 <77458410+PeterDBR27@users.noreply.github.com>
2022-12-10 19:08:29 -0300

    Add files via upload

---
+++ assets/js/converter.js
@@ -0,0 +1,288 @@
+// Use a função fetch() para obter os dados da API
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+    
+    var elements = document.querySelectorAll('.dollar-value');
+    
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("vip");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+});
+
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+
+    var elements = document.querySelectorAll('.dollar-value');
+
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("vip1");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+  });
+
+// Use a função fetch() para obter os dados da API
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+
+    var elements = document.querySelectorAll('.dollar-value');
+
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("vip+");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+  });
+
+// Use a função fetch() para obter os dados da API
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+
+    var elements = document.querySelectorAll('.dollar-value');
+
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("vip+1");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+  });
+
+// Use a função fetch() para obter os dados da API
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+
+    var elements = document.querySelectorAll('.dollar-value');
+
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("mvp");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+  });
+
+// Use a função fetch() para obter os dados da API
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+
+    var elements = document.querySelectorAll('.dollar-value');
+
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("mvp1");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+  });
+
+// Use a função fetch() para obter os dados da API
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+
+    var elements = document.querySelectorAll('.dollar-value');
+
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("mvp+");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+  });
+
+// Use a função fetch() para obter os dados da API
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+
+    var elements = document.querySelectorAll('.dollar-value');
+
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("mvp+1");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+  });
+
+// Use a função fetch() para obter os dados da API
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+
+    var elements = document.querySelectorAll('.dollar-value');
+
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("mvp++");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+  });
+
+// Use a função fetch() para obter os dados da API
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+  // Converta a resposta em um objeto JavaScript usando o método .json()
+  .then(response => response.json())
+  // Use o objeto para pegar o valor "bid"
+  .then(data => {
+    // Aqui, você tem acesso ao objeto com os dados da API
+    // e pode pegar o valor "bid" usando a sintaxe de acesso
+    // a propriedades de objeto em JavaScript
+    const bid = data.USDBRL.bid;
+
+    var elements = document.querySelectorAll('.dollar-value');
+
+    // Obtém o elemento HTML que contém o valor em dólar a ser convertido
+    var element = document.getElementById("mvp++1");
+
+    // Obtém o valor do elemento como uma string
+    var value = element.innerHTML;
+
+    // Converte a string em um número e calcula o valor em real
+    var real = parseFloat(value) * bid;
+
+    // Formata o valor em real como uma string
+    var realString = real.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Atualiza o valor do elemento com o valor convertido em real
+    element.innerHTML = realString;
+  });
